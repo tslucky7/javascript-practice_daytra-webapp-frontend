513b895b86638ff54f6b91ca24840cd3b2910e9d
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-04-18 15:58:52 +0900

    TODO名・担当者名で絞り込み機能の実装、一部jsの記載を可読性を上げるため変更

--- TODO-App/js/script.js
+++ TODO-App/js/script.js
@@ -1,5 +1,7 @@
 let todoList = [];
 
+let filterWord = "";
+
 const registerNewTodo = () => {
   //入力されたTODOの値を取得
   const newTodoName = document.getElementById("new-todo");
@@ -24,27 +26,35 @@ const removeTodoListElem = () => {
 }
 
 const appendTodoListElem = () => {
-  todoList.forEach((todo) => {
-    //TodoNameのtd要素を作成
-    const todoNameTdElem = document.createElement("td");
-    todoNameTdElem.textContent = todo.todoName;
-    //Personのtd要素を作成
-    const personTdElem = document.createElement("td");
-    personTdElem.textContent = todo.person;
-    //Deadlineのtd要素を作成
-    const deadlineTdElem = document.createElement("td");
-    deadlineTdElem.textContent = todo.deadline;
+  //TODOリストを全て削除
+  removeTodoListElem();
 
-    //tr要素を作成、td要素を追加
-    const trElem = document.createElement("tr");
-    trElem.appendChild(todoNameTdElem);
-    trElem.appendChild(personTdElem);
-    trElem.appendChild(deadlineTdElem);
+  todoList
+    .filter(
+      (todo) =>
+        todo.todoName.includes(filterWord) || todo.person.includes(filterWord)
+    )
+    .forEach((todo) => {
+      //TodoNameのtd要素を作成
+      const todoNameTdElem = document.createElement("td");
+      todoNameTdElem.textContent = todo.todoName;
+      //Personのtd要素を作成
+      const personTdElem = document.createElement("td");
+      personTdElem.textContent = todo.person;
+      //Deadlineのtd要素を作成
+      const deadlineTdElem = document.createElement("td");
+      deadlineTdElem.textContent = todo.deadline;
 
-    //tbody要素にtr要素を追加
-    const tbodyElem = document.getElementById("todo-list");
-    tbodyElem.appendChild(trElem);
-  });
+      //tr要素を作成、td要素を追加
+      const trElem = document.createElement("tr");
+      trElem.appendChild(todoNameTdElem);
+      trElem.appendChild(personTdElem);
+      trElem.appendChild(deadlineTdElem);
+
+      //tbody要素にtr要素を追加
+      const tbodyElem = document.getElementById("todo-list");
+      tbodyElem.appendChild(trElem);
+    });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -52,8 +62,14 @@ document.addEventListener("DOMContentLoaded", () => {
   registerButtonElem.addEventListener("click", () => {
     //入力されたTODOの値を取得して、TODOリストに新しいTODOを追加
     registerNewTodo();
-    //TODOリストを全て削除
-    removeTodoListElem();
+    //TODOリストを表示
+    appendTodoListElem();
+  });
+
+  //絞り込みが入力された場合のイベント定義
+  const filterInputElem = document.getElementById("filter");
+  filterInputElem.addEventListener("input", () => {
+    filterWord = filterInputElem.value;
     //TODOリストを表示
     appendTodoListElem();
   });
